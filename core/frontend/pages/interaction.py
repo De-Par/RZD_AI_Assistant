6dfa6739061b395254317cdefdac26db79db69ec
Enterprice <meshushkevich@gmail.com>
2023-09-10 09:43:54 +0300

    Commit 6: UI

--- core/frontend/pages/interaction.py
+++ core/frontend/pages/interaction.py
@@ -1,6 +1,5 @@
 import flet as ft
 from core.frontend import constants
-from core.backend.encoder_cos import AIWoman
 import whisper
 import pyaudio
 import wave
@@ -11,92 +10,6 @@ from sentence_transformers import SentenceTransformer, util
 import os
 import pyttsx3
 import cyrtranslit
-# class AIWoman:
-#     def __init__(self, dataset_path: str):
-#         self.data = pd.read_excel(dataset_path, index_col=0)
-#         if os.path.exists('./core/backend/encoded.npy'):
-#             encoded = pd.DataFrame(np.load('encoded.npy', allow_pickle=True))
-#         else:
-#             encoded = self.encode_dataset(self.data, np.arange(1, 6))
-#             encoded = encoded.to_numpy(copy=True)
-#             np.save('encoded.npy', encoded)
-#         self.encoded = encoded
-#         self.model = SentenceTransformer('distiluse-base-multilingual-cased')
-#
-#     def find_cos_sim(self, input_: str, train_model='говновоз'):
-#         return self.cos_sim(
-#             self.data,
-#             self.encoded,
-#             input_,
-#             np.arange(self.encoded.shape[0]),
-#             np.arange(2, self.encoded.shape[1]-1),
-#             self.model
-#         )
-#
-#     def encode_dataset(self, dataset, columns_to_encode):
-#         model = SentenceTransformer('distiluse-base-multilingual-cased')
-#         encoded = pd.DataFrame()
-#         for i in range(dataset.shape[1]):
-#             new_column = []
-#             if i in columns_to_encode:
-#                 for j in range(dataset.shape[0]):
-#                     try:
-#                         new_column.append(model.encode(dataset.iloc[j, i]))
-#                     except:
-#                         new_column.append(model.encode("NaN"))
-#             else:
-#                 new_column = dataset[dataset.columns[i]]
-#             encoded[dataset.columns[i]] = new_column
-#         return encoded
-#
-#     def cos_sim(self, initial, encoded, query, first_iterator, second_iterator, model):
-#
-#         query_encoded = model.encode(query, convert_to_tensor=True)
-#         mx = float('-inf')
-#         ans = []
-#         ansj = float('-inf')
-#
-#         for i in first_iterator:
-#             for j in second_iterator:
-#                 scores = util.pytorch_cos_sim(query_encoded, encoded.loc[i, j])
-#                 if scores > mx:
-#                     mx = scores
-#                     ans.clear()
-#                     ans.append(i)
-#                     ansj = j
-#                 elif scores == mx:
-#                     ans.append(i)
-#                     ansj = j
-#         if (ansj == initial.shape[1] - 3):
-#             text_to_speech(initial.iloc[ans[0], ansj + 1])
-#             print(initial.iloc[ans[0], ansj])
-#             return ans[0], ansj + 1
-#         else:
-#             print(*ans)
-#             #return second_attempt(initial, encoded, query, ans, ansj + 1)
-#
-#     def second_attempt(self, initial, encoded, query, first_iterator, column):
-#         wrong = []
-#         print(column)
-#         if column >= initial.shape[1]:
-#             # Ошибка разбора (не удалось установить причину)
-#             return 'failure', None
-#         for reason in first_iterator:
-#             if initial.iloc[reason, column] not in wrong:
-#                 return 'get more', 'Правда ли, что ' + initial.iloc[reason, column] + '?'
-#                 inp = input()
-#                 if (inp == 'нет'):
-#                     wrong.append(initial.iloc[reason, column])
-#                 elif (inp == 'да'):
-#                     if (column == len(initial.columns) - 3):
-#                         return reason, column + 1
-#                     else:
-#                         new_indexes = []
-#                         for i in first_iterator:
-#                             if initial.iloc[reason, column] == initial.iloc[i, column]:
-#                                 new_indexes.append(i)
-#                         return second_attempt(initial, encoded, query, new_indexes, column + 1)
-
 
 
 class AIAssistant:
